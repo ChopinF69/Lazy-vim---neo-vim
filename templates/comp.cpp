7a05c74e65d71589cc389ef9da736b75cbc3d989
ChopinF69 <your_email@example.com>
2024-08-14 10:18:11 +0300

    Full deploy

---
+++ templates/comp.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+std::string file = "prime";
+std::ifstream fin(file + ".in");
+std::ofstream fout(file + ".out");
+// #define fin std::cin
+// #define fout std::cout
+int32_t main(int32_t argc, char *argv[]) {
+  ios_base::sync_with_stdio(false);
+  fin.tie(0);
+  fout.tie(0);
+  return 0;
+}
